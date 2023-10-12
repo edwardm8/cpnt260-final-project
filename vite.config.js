f845b4e23776e3cd6b1e4563eac0fbc48b1a4a37
edwardm8 <edwardm825@gmail.com>
2023-10-12 17:24:33 -0600

    update readme and static page

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import { defineConfig } from 'vite';
+import {resolve} from 'path';
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname,'./dist')
+
+export default {
+	root,
+	base: './',
+	publicDir: '../static/',
+	server:
+	{
+			host: true,
+			open: !isCodeSandbox // Open if it's not a CodeSandbox
+	},
+	build: {
+			outDir,
+			emptyOutDir: true,
+			sounrcemap: true,
+			rollupOptions: {
+				input: {
+					main: resolve(root, 'index.html'),
+					contact: resolve(root, 'contact.html'),
+					home: resolve(root,'home.html'),
+					gallery: resolve(root,'gallery.html'),
+				}
+			}
+	}
+}
